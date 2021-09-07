abda28de6afa36ddeb4e1c4393e32e411f9dee31
Froila <froila.molinero@gmail.com>
2021-09-07 12:27:49 +0200

    Normalize the solutions from the input file

--- tests/test_words.py
+++ tests/test_words.py
@@ -2,23 +2,26 @@ from pathlib import Path
 
 import pytest
 
-from cruziwords.words import Word, WordsCorpus
+from cruziwords.words import Word, WordsCorpus, normalize
 
 
 @pytest.fixture
 def words_csv(tmp_path) -> Path:
     csv_content = """Capital of Afghanistan,KABUL
-European Capital,BERLIN,MADRID"""
+European Capital,BERLIN,MADRID
+Spain in Spanish,españa
+To be in French,ÊTRE
+Lice in German,Läuse"""
     csv_file = tmp_path / "words.csv"
-    csv_file.write_text(csv_content)
+    csv_file.write_text(csv_content, encoding="utf-8")
     return csv_file
 
 
 def test_words(words_csv: Path):
     words = WordsCorpus.from_csv_file(str(words_csv))
 
-    assert len(words) == 3
-    assert len(set(words)) == 3
+    assert len(words) == 6
+    assert len(set(words)) == 6
 
     assert any(word.solution == "BERLIN" for word in words)
     assert any(word.solution == "MADRID" for word in words)
@@ -41,3 +44,15 @@ def test_words_containing_letter(words_csv: Path):
 
     assert {(word.solution, i) for word, i in words.containing("I")} == {("BERLIN", 4), ("MADRID", 4)}
     assert not set(words.containing("X"))
+
+
+def test_normalisation(words_csv: Path):
+    words = WordsCorpus.from_csv_file(words_csv)
+    accented_string = "àéêhelloñçëïßäöü"
+
+    assert normalize(accented_string) == "AEEHELLOÑÇËÏSSAEOEUE"
+    assert normalize(accented_string) != "AEEHELLOÑÇËÏßAEOEUE"
+
+    assert any(word.solution == "ESPAÑA" for word in words)
+    assert any(word.solution == "ETRE" for word in words)
+    assert any(word.solution == "LAEUSE" for word in words)
